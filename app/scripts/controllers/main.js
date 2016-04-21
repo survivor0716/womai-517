7e00a75e232bc60552bdf34c962aa4402ef0a8e7
Survivor <dongchen1022@gmail.com>
2016-04-21 16:42:20 +0800

    初始架构

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name womai517App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the womai517App
+ */
+angular.module('womai517App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
